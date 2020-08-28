aa3b2a1f6d8a3c8d8f7c31b8f945878fcb3a9dff
Emma Forson <68652074+emmalnakforson@users.noreply.github.com>
2020-08-28 17:40:52 +0000

    done with quiz

--- src/App.js
+++ src/App.js
@@ -6,27 +6,29 @@ const data = [
   { id: 1, value: 0 },
   { id: 2, value: 0 },
   { id: 3, value: 0 },
+  //{ id: 4, value: 0 },
 ];
 
 // Counter Component
 class Counter extends Component {
   render() {
 
-    const { value } = this.props;
+    const { value, increplus, decreplus  } = this.props;
     return (
-      <div>
-         function onIncrement(value){
-           data.includes(true)
+      //<div>
+        // function onIncrement(value){
+           //data.includes(true)
       //value = {this.render} 
-         }
-    </div>
+         //}
+    //</div>
       <div className="counter">
         <b>{value}</b>
         <div className="counter-controls">
-          <button className="button is-danger is-small">-</button>
-          <button className="button is-success is-small">+</button>
+          <button tap = {decreplus} className="button is-danger is-small">-</button>
+          <button tap = {increplus} className="button is-success is-small">+</button>
         </div>
       </div>
+    );
   }
 }
 
@@ -34,16 +36,48 @@ class Counter extends Component {
 class App extends Component {
   constructor(props, context) {
     super(props, context);
-    data (props, context);
+    this.state= {
+    //data (props, context);
+    data: [{id:1, value: 0},
+           {id: 2, value:0},
+           {id:3, value:0},
+           {id:4, value:0},
+   ]
 
   }
+}
+
+onIncrement = (id) => {
+  this.setState(previousState => ({ data: previousState.data.map(
+    m => m.id === id? {id:m.id, value: m.value++}: m
+  )}) )
+//(counter,increment) => {
+ // const createdValue = counter.value + increment;
+  //this.setState(
+   // {
+    //  data: data.map (item => {item.value = item.id === counter.id?createdValue:
+      //item.value;
+   // return item;})
+   // }
+  //)
+}
+onDecrement = (id) => {
+  this.setState(previousState => ({ data: previousState.data.map(
+    m => m.id === id? {id:m.id, value: m.value--}: m
+  )}) )
+}
 
   render() {
     return (
       <div>
-        {data.map((counter) => (
-          <Counter key={counter.id} value={counter.value} />
-        ))}
+        {this.state.data.map((counter) => (
+          <Counter //key={counter.id} value={counter.value} />
+          increplus = {() => this.onIncrement(counter.id)}
+          decreplus = {() => this.onDecrement(counter.id)}
+          key = {counter.id} value = {counter.value}/>
+          ))
+        }
+
       </div>
     );
   }
